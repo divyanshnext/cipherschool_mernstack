ff2290b8d23e5d0daaf2a6ba76cd1b94bd361d93
divyansh kr. chaturvedi <divyanshchaturvedi317@gmail.com>
2024-06-20 17:00:48 +0530

    new react component added

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,31 +1,10 @@
-// import logo from './logo.svg';
-import './App.css';
 import { Component } from "react";
+import ToDoScreen from "./screens/ToDoScreens";
 
 class App extends Component {
+    render(){
+        return <ToDoScreen />;
+    }
 
-  name = "CipherSchool";
-  author = "divyansh"
-
-  render() {
-    return (
-      <>
-      <div>
-        <h1>This is H1 JSX {this.name} </h1>
-      </div>
-      <div>
-        <p>this is paragraph in JSX by {this.author}</p>
-      </div>
-      </>
-  )
-  }
 }
-
 export default App;
-
-
-// notes for jsx
-
-// taking about ui related information
-// in <h1> style - we put two bracket first bracket is to tell that we gonna write js code in it 
-//in jsx only one parent <div> should be there and inside that we have to write all div
