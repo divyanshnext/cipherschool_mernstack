749f12ef65c141f5e7dabb13fa34052655007024
divyansh kr. chaturvedi <divyanshchaturvedi317@gmail.com>
2024-06-20 17:50:30 +0530

    some modifications

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import ToDoScreen from "./screens/ToDoScreens";
+// import all the components which u gonna use here just like here i have imported ToDoScreen
 
 class App extends Component {
     render(){
--- react-app/src/screens/ToDoScreens.js
+++ react-app/src/screens/ToDoScreens.js
@@ -1,9 +1,35 @@
 import { Component } from "react";
 
 class ToDoScreen extends Component {
+
+    state = {
+        taskCount: 0,
+    };
+
     render() {
-        return <h1 className="ui heading center">To Do List</h1>;
+        return (
+            <div className = "screen">
+                <h1 className="ui heading center">To Do List</h1>
+                <div>
+                <button onClick={(e)=> {
+                    //we have to pass an anonymous function here to use event handller 
+                    this.setState({
+                        ...this.state, 
+                        taskCount: this.state.taskCount + 1
+                    });
+                    console.log(this.state.taskCount); 
+                    // to use state object we have used it 
+                    // console.log("Add Task button was clicked.")
+                }}
+                className="ui secondary button"> Add Task
+                </button>
+                <p>The number of tasks are : {this.state.taskCount}</p>
+                {/* whenever we have a variable which is constantly changing and the change is the variable can calls the content on the screen to change they need to be stores in a very special kind of variable named //state */}
+                </div>
+            </div>
+        );
     }
 }
 
-export default ToDoScreen;
+export default ToDoScreen; 
+//agar export nhi kroge toh chlega hi nhi app bnake export krna padta hai 
