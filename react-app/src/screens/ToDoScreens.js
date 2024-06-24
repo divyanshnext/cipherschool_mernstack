ec0a3eeaae104fbc28c5f327035543acf31dea57
divyansh kr. chaturvedi <divyanshchaturvedi317@gmail.com>
2024-06-24 16:48:31 +0530

    new files adding

--- react-app/src/screens/ToDoScreens.js
+++ react-app/src/screens/ToDoScreens.js
@@ -1,35 +1,71 @@
-import { Component } from "react";
+import {useState} from 'react';
+import Task from "../components/Task";
 
-class ToDoScreen extends Component {
 
-    state = {
-        taskCount: 0,
-    };
+//class based component
 
-    render() {
-        return (
-            <div className = "screen">
-                <h1 className="ui heading center">To Do List</h1>
-                <div>
-                <button onClick={(e)=> {
-                    //we have to pass an anonymous function here to use event handller 
-                    this.setState({
-                        ...this.state, 
-                        taskCount: this.state.taskCount + 1
-                    });
-                    console.log(this.state.taskCount); 
-                    // to use state object we have used it 
-                    // console.log("Add Task button was clicked.")
-                }}
-                className="ui secondary button"> Add Task
-                </button>
-                <p>The number of tasks are : {this.state.taskCount}</p>
-                {/* whenever we have a variable which is constantly changing and the change is the variable can calls the content on the screen to change they need to be stores in a very special kind of variable named //state */}
-                </div>
-            </div>
-        );
-    }
-}
+// class ToDoScreen extends Component {
+//     state = {
+//         taskCount: 0,
+//     };
+
+//     render() {
+//         return (
+//             <div className = "screen">
+//                 <h1 className="ui heading center">To Do List</h1>
+//                 <div>
+//                 <button onClick={(e)=> {
+//                     //we have to pass an anonymous function here to use event handller 
+//                     this.setState({
+//                         ...this.state, 
+//                         taskCount: this.state.taskCount + 1
+//                     });
+//                     console.log(this.state.taskCount); 
+//                     // to use state object we have used it 
+//                     // console.log("Add Task button was clicked.")
+//                 }}
+//                 className="ui secondary button"> Add Task
+//                 </button>
+//                 <p>The number of tasks are : {this.state.taskCount}</p>
+//                 {/* whenever we have a variable which is constantly changing and the change is the variable can calls the content on the screen to change they need to be stores in a very special kind of variable named //state */}
+//                 </div>
+//             </div>
+//         );
+//     }
+// }
+
+
+
+//function based component
+const ToDoScreen = () => {
+    // const[taskCount,setTaskCount] = useState(0);
+    const[taskList,setTaskList] = useState([]);
+    return (
+                    <div className = "screen">
+                        <h1 className="ui heading center">To Do List</h1>
+                        <div>
+                        <button onClick={(e)=> {
+                            setTaskList([
+                                ...taskList,
+                                {
+                                    title : "go to gym",
+                                    description : "going to gym is goood for muscle growth",
+                                },
+                            ]);
+                        }}
+                        className="ui secondary button"> Add Task
+
+                        </button>
+                        
+                        {taskList.map((task) => (
+                            <Task/>
+                    ))}
+                        
+                        </div>
+                    </div>
+                );
+
+};
 
 export default ToDoScreen; 
 //agar export nhi kroge toh chlega hi nhi app bnake export krna padta hai 
