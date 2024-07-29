261eadaa0f5038a99f2edcc792c53bee303b40c0
divyansh kr. chaturvedi <divyanshchaturvedi317@gmail.com>
2024-07-29 15:16:30 +0530

    mongoDB added

--- node/math/src/app.js
+++ node/math/src/app.js
@@ -1,10 +1,17 @@
-const http = require('http');
 
-const server = http.createServer((req,res)=> {
-    res.write("this is some response from your first node js server");
-    res.end();
+const express = require("express");
+const app = express();
+
+app.get("/",(req,res) => {
+    res.send("this is some response from your firsty node js server ");
 });
 
-server.listen(3000, ()=>{
-    console.log('server is running');
+app.get("/add",(req,res) => {
+    let {a: firstNumber,b:secondNumber} = req.query;
+    let sum = parseInt(firstNumber) + parseInt(secondNumber);
+    res.send({sum});
+});
+
+app.listen(8080, () => {
+    console.log('Server is running');
 });
